f607fd50b0ea2e161f39d4c4f2cce45c63b2c135
Joshua Parco <jacparco@gmail.com>
2021-03-30 13:25:02 -0600

    initial commit

---
+++ autocomplete_test.rb
@@ -0,0 +1,19 @@
+require "selenium-webdriver"
+require "webdrivers"
+
+driver = Selenium::WebDriver.for :chrome
+driver.navigate.to "https://formy-project.herokuapp.com/autocomplete"
+
+autocomplete = driver.find_element(id: 'autocomplete')
+autocomplete.send_keys('1555 Park Blvd, Palo Alto, CA, USA')
+
+#Implicit 
+#driver.manage.timeoutes.implicit_wait = 5
+
+#Explicit
+wait = Selenium::WebDriver::Wait.new(timeout: 5)
+wait.until { driver.find_element(class: 'pac-item').displayed? }
+
+autocomplete_result = driver.find_element(class: "pac-item")
+autocomplete_result.click
+
